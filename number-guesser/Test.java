1dff0169d23188338f10ca5dbe5341f40b11e15b
zerok1986 <zerok1986@mail.com>
2021-04-15 00:41:20 +0200

    Create test in Java

---
+++ number-guesser/Test.java
@@ -0,0 +1,7 @@
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
