fae37a43c403fd990c5051f336b13d7c405908ac
zerok1986 <zerok1986@mail.com>
2021-04-18 18:33:46 +0200

    Modify the import call

--- number-guesser/Test.java
+++ number-guesser/Test.java
@@ -1,5 +1,3 @@
-import jdk.javadoc.internal.doclets.formats.html.resources.standard;
-
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
