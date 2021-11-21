9208ef786696474c885b3f61f40b629d1b8c3bd1
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-21 03:01:53 +0100

    Add typescript and rust projects

---
+++ rust-projects/hello_cargo/target/debug/deps/hello_cargo-f83b7d1a09de7645.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/maaba/OneDrive/Documentos/Miky/2zero21/projects/rust-projects/hello_cargo/target/debug/deps/hello_cargo-f83b7d1a09de7645: src/main.rs
+
+/mnt/c/Users/maaba/OneDrive/Documentos/Miky/2zero21/projects/rust-projects/hello_cargo/target/debug/deps/hello_cargo-f83b7d1a09de7645.d: src/main.rs
+
+src/main.rs:
